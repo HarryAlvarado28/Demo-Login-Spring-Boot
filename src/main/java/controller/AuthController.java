5f450e5f77659a438083282ef2bddadc91d8760d
Harry Alvarado <harry2alvarado8@hotmail.com>
2023-11-12 17:03:20 -0500

    Registro base de datos

--- src/main/java/controller/AuthController.java
+++ src/main/java/controller/AuthController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,21 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+//@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
+    private static Logger logger = LoggerFactory.getLogger(AuthController.class);
     private final AuthService authService;
 
     @PostMapping(value = "login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
+        logger.info("Mensaje desde un INFO LOGIN");
         return ResponseEntity.ok(authService.login(request));
     }
 
     @PostMapping(value = "register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
+        logger.info("Mensaje desde un INFO REGISTER");
         return ResponseEntity.ok(authService.register(request));
     }
-
 
 }
---
+++ src/main/java/jwt/JwtService.java
@@ -0,0 +1,35 @@
+package jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+
+public class JwtService {
+
+    private static  final String SECRET_KEY = "123412323564563564565685467456";
+    public String getToken(UserDetails user) {
+        return getTokenValue(new HashMap<>(), user);
+    }
+
+    private String getTokenValue(HashMap<String, Object> extraClaims, UserDetails user) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
